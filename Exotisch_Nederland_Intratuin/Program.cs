30654c6383b9a394c4a6f64c259f544044b14865
RSLT <RSLT@DESKTOP-IC9BCKP>
2024-09-26 10:02:19 +0200

    Add project files.

---
+++ Exotisch_Nederland_Intratuin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exotisch_Nederland_Intratuin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
