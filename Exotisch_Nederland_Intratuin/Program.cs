013d1a8b74efedfdee669e3cda190f7f17b88a42
Tygo van Eerd <vaneerdtygo@gmail.com>
2024-10-14 17:48:06 +0200

    Revert "Classes aangemaakt en ingevuld"
    This reverts commit 7ea63463759301d17903b61e50e84e1ca52dfca5.

--- Exotisch_Nederland_Intratuin/Program.cs
+++ Exotisch_Nederland_Intratuin/Program.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exotisch_Nederland_Intratuin {
-    internal class Program {
-        static void Main(string[] args) {
-
+namespace Exotisch_Nederland_Intratuin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
         }
     }
 }
